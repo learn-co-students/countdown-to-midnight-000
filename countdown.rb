67a4f0d8b32df8d9290f70fce31a0ed72f90d7da
chaplonglau <chaplonglau@gmail.com>
2015-05-24 22:44:11 -0400

    countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  
+  while num>0
+    puts "#{num} SECOND(S)!"
+    num-=1
+
+  end
+  if num==0
+    return "0 left. HAPPY NEW YEAR!"
+  end
+end
+
+
+def countdown_with_sleep(num)
+  
+  while num>0
+    puts "#{num} SECOND(S)!"
+    num-=1
+    sleep 1
+
+  end
+  if num==0
+    return "0 left. HAPPY NEW YEAR!"
+  end
 end
