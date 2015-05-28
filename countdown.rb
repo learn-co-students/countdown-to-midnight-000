fdc53ef8da89203fed499dbe1e8cde6248882381
cornerstonian <vois.cornerstone@gmail.com>
2015-05-28 15:11:23 -0400

    fin

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(time_left)
+  while time_left > 0
+    puts "#{time_left} SECOND(S)!"
+    time_left -= 1
+    countdown_with_sleep
+  end
+  "0 left. HAPPY NEW YEAR!" 
+end
+
+def countdown_with_sleep(seconds=1)
+  sleep(seconds)
 end
