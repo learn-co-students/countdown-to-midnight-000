bbfebee1c1414032818fed6084d8deb1632593b0
Rigz777 <rigzgoodmusic@gmail.com>
2015-05-28 14:50:15 -0400

    countdown complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(timer)
+  while timer > 0
+    puts "#{timer} SECOND(S)!"
+    timer -= 1     
+  end
+"0 left. HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sleep)
+  sleep 5
 end
