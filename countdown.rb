3b68d303c6edcc93400c68fdfc6fe00af1c8f65b
Rigz777 <rigzgoodmusic@gmail.com>
2015-05-28 15:26:22 -0400

    corrected

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,14 @@
 #write your code here
 
 def countdown(timer)
-  while timer > 0
+  while timer > 0 
     puts "#{timer} SECOND(S)!"
-    timer -= 1     
+    timer -= 1
+    countdown_with_sleep    
   end
 "0 left. HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(sleep)
-  sleep 5
+def countdown_with_sleep(seconds=1)
+  sleep(seconds)
 end
