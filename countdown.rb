0ed59e4e6eb4984e42d9fd30d3ccab67a3aa4edb
bidah <bartoindahouse@gmail.com>
2015-06-05 09:07:08 -0400

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  @twelve_seconds = seconds
+  def its_twelve?
+    @twelve_seconds == 12 ? true : false
+  end
+
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  
+  if its_twelve?
+    "HAPPY NEW YEAR!" if seconds == 0
+  end
+end
+
+
+def countdown_with_sleep(seconds)
+  sleep(5)
+  countdown(seconds)
 end
