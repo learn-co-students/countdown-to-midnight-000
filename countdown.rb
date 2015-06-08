2857e2f34af8af1b782de52a66d96f2c1b9eccd4
sammarcus <sam.marcus@me.com>
2015-06-07 19:59:51 -0500

    done with sleep delay

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,22 @@ def countdown(input_time)
   while i > 0
     puts "#{i} SECOND(S)!"
     i -= 1
+    # countdown_with_sleep
   end
-  if i = 0
+  if i == 0
     return "HAPPY NEW YEAR!"
   end
 end
 
-def countdown_with_sleep
-  # pause
+def countdown_with_sleep(input_time)
+  i = input_time
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep(1)
+    # countdown_with_sleep
+  end
+  if i == 0
+    return "HAPPY NEW YEAR!"
+  end
 end
