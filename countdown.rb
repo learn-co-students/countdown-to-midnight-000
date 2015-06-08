c45115cc0e02a9c012c27f50ed25c20770cabf8c
sammarcus <sam.marcus@me.com>
2015-06-07 19:49:55 -0500

    without sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+def countdown(input_time)
+  i = input_time
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  if i = 0
+    return "HAPPY NEW YEAR!"
+  end
+end
 
-def countdown
+def countdown_with_sleep
+  # pause
 end
