40f9a31c75ced5458f23fff76beeba399691f981
aeremin <aeremin@maxus.ru>
2015-06-08 15:07:49 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  counter = n
+  while counter > 0
+	puts "#{counter} SECOND(S)!"
+	counter -= 1
+  end	
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep 6
 end
