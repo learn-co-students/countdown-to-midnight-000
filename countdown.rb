fbda569f0fad2527e072b27d929875f439c989b4
brianduggan <brian.e.duggan@icloud.com>
2015-06-14 16:13:28 -0400

    add 2 countdown methods

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  x = seconds
+  until x == 0
+    puts "#{x} SECOND(S)!"
+    x-=1
+  end
+  if x
+    return "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep(seconds)
+  x = seconds
+  until x == 0
+    puts "#{x} SECOND(S)!"
+    sleep 1
+    x-=1
+  end
+  if seconds > 10
+    return "HAPPY NEW YEAR!"
+  end
+end
+
