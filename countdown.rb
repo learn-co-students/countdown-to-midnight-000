4cffcbc00ce323f2c0a72109b088e33d81d504e3
dpinkus <david@pinkus.com>
2015-09-16 20:44:30 -0700

    looping and sleeping

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(numsecs)
+  # 33 days
+  x = numsecs
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(numsecs)
+  # 33 days
+  x = numsecs
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
