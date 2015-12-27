8d578e432eaf796477137ec22518031e090d7c9c
Patti O'Neill <pdoneill26@gmail.com>
2015-12-27 13:34:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  i = n
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  i = n
+  while i > 0
+  sleep(1)
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+    return "HAPPY NEW YEAR!"
 end
