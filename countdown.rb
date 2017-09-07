44d3dea4bfc6ef7dadcacc02d92c9b0e9a36365f
Alex Katalkin <alexey.katalkin@gmail.com>
2017-09-07 16:40:54 +0000

    Counting

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (argument)
+  while argument > 0
+    puts "#{argument} SECOND(S)!"
+    argument -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (argument)
+  while argument > 0
+    puts "#{argument} SECOND(S)!"
+    sleep(1)
+    argument -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
